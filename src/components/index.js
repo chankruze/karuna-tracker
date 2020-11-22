9f54a0cdc4fc30b0b3c9cf4a5b40668077df7fb4
chankruze <chankruze@gmail.com>
2020-11-22 11:08:29 +0530

    Add Components to Common Import
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Sun Nov 22 2020 08:32:23 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+export { default as Cards } from "./Cards/Cards";
+export { default as Chart } from "./Chart/Chart";
+export { default as CountryPicker } from "./CountryPicker/CountryPicker";
