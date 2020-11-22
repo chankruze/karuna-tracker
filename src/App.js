ea7eb8577c915a0e7570109a58f21a90bef5c19b
chankruze <chankruze@gmail.com>
2020-11-22 11:08:58 +0530

    Add Logo & Components to App
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import "./App.css";
+import React from "react";
+import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
+import karunaLogo from "./images/logo.png";
+import styles from "./App.module.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    // update new data
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    // update new data
+    this.setState({ data: fetchedData, country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img src={karunaLogo} className={styles.image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
